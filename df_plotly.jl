1de5534c11b46a0cf01617a25900bb509e8284f2
mariel-rs <99100374+mariel-rs@users.noreply.github.com>
2022-12-29 15:10:49 -0600

    Add experimental script using PlotlyJS

---
+++ df_plotly.jl
@@ -0,0 +1,7 @@
+using PlotlyJS
+using CSV, DataFrames
+
+# Use julia -i df_plotly.jl command to run the script interactively
+df = dataset(DataFrame, "tips")
+example_plot = plot(df, y=:tip, facet_row=:sex, facet_col=:smoker, color=:day, kind="violin")
+display(example_plot)
