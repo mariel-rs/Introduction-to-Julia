a5284e2cae99fa58968516c5d744247418e2d94b
mariel-rs <99100374+mariel-rs@users.noreply.github.com>
2022-12-30 22:13:18 -0600

    Cosmetic update for improving readibility

--- df_plotly.jl
+++ df_plotly.jl
@@ -3,5 +3,6 @@ using CSV, DataFrames
 
 # Use julia -i df_plotly.jl command to run the script interactively
 df = dataset(DataFrame, "tips")
-example_plot = plot(df, y=:tip, facet_row=:sex, facet_col=:smoker, color=:day, kind="violin")
+example_plot = plot(df, y = :tip, facet_row = :sex, facet_col = :smoker, 
+                    color = :day, kind = "violin")
 display(example_plot)
